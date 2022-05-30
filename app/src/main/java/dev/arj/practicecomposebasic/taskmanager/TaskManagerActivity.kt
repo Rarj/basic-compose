1003d3d9ce7e855341b107aa5ef111b7ed614618
Rio Arjuna <ri0arjuna@programmer.net>
2022-05-30 22:15:49 +0700

    extraxt strings.xml Task Manager

--- app/src/main/java/dev/arj/practicecomposebasic/taskmanager/TaskManagerActivity.kt
+++ app/src/main/java/dev/arj/practicecomposebasic/taskmanager/TaskManagerActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,12 +49,12 @@ fun TaskCompleteCompose() {
 
         Image(painter = painter, contentDescription = null, alignment = Alignment.Center)
         Text(
-            text = "All tasks completed",
+            text = stringResource(R.string.task_complete),
             fontSize = 24.sp,
             modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
         )
         Text(
-            text = "Nice work!",
+            text = stringResource(R.string.task_subtitle),
             fontSize = 16.sp
         )
     }

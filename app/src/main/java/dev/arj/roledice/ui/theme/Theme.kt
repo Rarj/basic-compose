1b5d2e3ad23e771b5026875335824dc58be8e65b
Rio Arjuna <ri0arjuna@programmer.net>
2022-07-20 22:21:00 +0700

    roll the dice and set it into image depend on result

--- app/src/main/java/dev/arj/roledice/ui/theme/Theme.kt
+++ app/src/main/java/dev/arj/roledice/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun RoleDiceTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun DiceRollerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
